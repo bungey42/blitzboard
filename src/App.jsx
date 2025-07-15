29431ebb9782728c0a6f836019151e2929b87df0
bungey42 <joe.bungey@mustardjobs.co.uk>
2025-07-15 08:59:54 +0100

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,8 @@
 
-import React from "react";
-import Leaderboard from "./components/Leaderboard";
-import Timer from "./components/Timer";
-import AdminPanel from "./components/AdminPanel";
-import "./App.css";
-
 export default function App() {
   return (
-    <div className="min-h-screen bg-gray-100 text-gray-900 p-4">
-      <header className="flex justify-between items-center mb-6">
-        <img src="/blitzboard/logo.svg" alt="mustard logo" className="h-12" />
-        <Timer />
-      </header>
-      <Leaderboard />
-      <AdminPanel />
+    <div style={{ padding: '2rem', fontSize: '2rem' }}>
+      ✅ Mustard Blitz Is Working!
     </div>
   );
 }
