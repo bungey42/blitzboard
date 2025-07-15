9636e75da43315bf1076bd9ad9a6258fb35e7f67
bungey42 <joe.bungey@mustardjobs.co.uk>
2025-07-15 08:42:14 +0100

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,13 @@ import Leaderboard from "./components/Leaderboard";
 import Timer from "./components/Timer";
 import AdminPanel from "./components/AdminPanel";
 import "./App.css";
+import logo from "/logo.svg";
 
 export default function App() {
   return (
     <div className="min-h-screen bg-gray-100 text-gray-900 p-4">
       <header className="flex justify-between items-center mb-6">
-        <img src="/logo.svg" alt="mustard logo" className="h-12" />
+        <img src={logo} alt="mustard logo" className="h-12" />
         <Timer />
       </header>
       <Leaderboard />
